dfb069856043fb379572647c9ab5e8f6c4c69704
Your Name <you@example.com>
2026-03-30 10:52:46 +0900

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.__SUPABASE_CONFIG__ = {
-  url: '',
-  anonKey: '',
+  url: 'https://wqmgiucnapcilunleirf.supabase.co',
+  anonKey: 'sb_publishable_fahoyXkP5i1ORKOJzRtP5w_67V5j7aw',
   leaderboardTable: 'leaderboard_scores',
 };
